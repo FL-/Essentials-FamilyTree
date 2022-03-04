3174334eb5422f48fe6a9bdacd8dff6ab2232aca
FL <fl50000@hotmail.com>
2022-03-04 05:26:25 -0300

    Essentials v17-18.1 version

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Family Tree
 This script is for Pokémon Essentials. It displays a sixth page at pokémon summary showing a little info about the pokémon mother, father, grandmothers and grandfathers if the pokémon has any.
 
+## Screens
+![](Screens/screen.png)
+![](Screens/screen2.png)
+
 ## Compatibility
-Works only on Essentials versions v16, v16.1 and v16.2. To see other Essentials versions, look at [all branches](../../branches/all).
+Works only on Essentials versions v17, v17.1, v17.2, v18 and v18.1. To see other Essentials versions, look at [all branches](../../branches/all).
 
 ## Installation
 Follow the [Script](/Script.rb) instructions. The sample script files are in [Files folder](/Files).
--- Script.rb
+++ Script.rb
@@ -8,89 +8,145 @@
 #
 #===============================================================================
 #
-# To this script works, put it above main, put a 512x384 background for this
-# screen in "Graphics/Pictures/summary6" and in "Graphics/Pictures/summaryEgg6".
-# This last one is only necessary if SHOWFAMILYEGG is true. You also need to
-# update the below pictures in order to reflect the summary icon change:
-# - summary1
-# - summary2
-# - summary3
-# - summary4
-# - summary4details
-# - summary5
+# INSTALLATION FOR ESSENTIALS VERSION 18
 #
-# -At PokemonDayCare, before line '$Trainer.party[$Trainer.party.length]=egg'
+# To this script works, put it above PSystem_System. Put a 512x384 background
+# for this screen in "Graphics/Pictures/Summary/" as "bg_6" and as "bg_6_egg".
+# This last one is only necessary if SHOWFAMILYEGG is true. You also need to
+# update the below pictures on same folder in order to reflect the summary
+# icon change:
+# - bg_1
+# - bg_2
+# - bg_3
+# - bg_4
+# - bg_movedetail
+# - bg_5
+#
+# -At PField_DayCare, before line '$Trainer.party[$Trainer.party.length]=egg'
 # add line 'egg.family = PokemonFamily.new(egg, father, mother)'
 #
-# -At PokemonSummary, change both lines '@page=4 if @page>4'
-# to '@page=5 if @page>5'
+# -At PScreen_Summary, change both lines '@page = 5 if @page>5'
+# to '@page=6 if @page>6'
 #
-# -Before line 'if Input.trigger?(Input::UP) && @partyindex>0'
-# add line 'handleInputsEgg'
+# -Before line 'if Input.trigger?(Input::A)' add line 'handleInputsEgg'
 #
-# -Change line 'if @page!=0' to 'if @page!=0 && !(SHOWFAMILYEGG && @page==5)'
+# -After line 'when 5; drawPageFive' add 'when 6; drawPageSix'
 #
-# -After line 'drawPageFive(@pokemon)' add
+# -Change line '_INTL("RIBBONS")][page-1]' into:
 #
-# when 5
-#  drawPageSix(@pokemon)
+# _INTL("RIBBONS"),
+# _INTL("FAMILY TREE")][page-1]
+#
+# -Change both lines 
+# 'if @party[newindex] && (@page==1 || !@party[newindex].egg?)' into:
+#
+# if @party[newindex] && 
+#   (@page==1 || !@party[newindex].egg? || (@page==6 && SHOWFAMILYEGG))
+#
+# -Change both
+# 
+#  pbSEStop; pbPlayCry(@pokemon)
+#  @ribbonOffset = 0
+#  dorefresh = true
+#
+# into:
+#
+#  pbSEStop; pbPlayCry(@pokemon)
+#  @ribbonOffset = 0
+#  if SHOWFAMILYEGG && @pokemon.isEgg? && @page==6
+#    dorefresh = false
+#    drawPageSix
+#  else
+#    dorefresh = true
+#  end
+#
+#===============================================================================
+#
+# INSTALLATION FOR ESSENTIALS VERSION 17
+#
+# To this script works, put it above PSystem_System. Put a 512x384 background
+# for this screen in "Graphics/Pictures/Summary/" as "bg_6" and as "bg_6_egg".
+# This last one is only necessary if SHOWFAMILYEGG is true. You also need to
+# update the below pictures on same folder in order to reflect the summary
+# icon change:
+# - bg_1
+# - bg_2
+# - bg_3
+# - bg_4
+# - bg_movedetail
+# - bg_5
+#
+# -At PField_DayCare, before line '$Trainer.party[$Trainer.party.length]=egg'
+# add line 'egg.family = PokemonFamily.new(egg, father, mother)'
+#
+# -At PScreen_Summary, change both lines '@page = 5 if @page>5'
+# to '@page=6 if @page>6'
+#
+# -Before line 'if Input.trigger?(Input::A)' add line 'handleInputsEgg'
+#
+# -After line 'when 5; drawPageFive' add 'when 6; drawPageSix'
+#
+# -Change line '_INTL("RIBBONS")][page-1]' into:
+#
+# _INTL("RIBBONS"),
+# _INTL("FAMILY TREE")][page-1]
+#
+# -Change both lines 
+# 'if @party[newindex] && (@page==1 || !@party[newindex].egg?)' into:
+#
+# if @party[newindex] && 
+#   (@page==1 || !@party[newindex].egg? || (@page==6 && SHOWFAMILYEGG))
+#
+# -Change both
+# 
+#  pbSEStop; pbPlayCry(@pokemon)
+#  @ribbonOffset = 0
+#  dorefresh = true
+#
+# into:
+#
+#  pbSEStop; pbPlayCry(@pokemon)
+#  @ribbonOffset = 0
+#  if SHOWFAMILYEGG && @pokemon.isEgg? && @page==6
+#    dorefresh = false
+#    drawPageSix
+#  else
+#    dorefresh = true
+#  end
 #
 #===============================================================================
 
-class PokemonSummaryScene
+class PokemonSummary_Scene
   SHOWFAMILYEGG = true # when true, family tree is also showed in egg screen.
-  
-  def drawPageSix(pokemon)
+
+  def drawPageSix
     overlay=@sprites["overlay"].bitmap
-    overlay.clear
-    @sprites["background"].setBitmap(@pokemon.isEgg? ? 
-        "Graphics/Pictures/summaryEgg6" : "Graphics/Pictures/summary6")
-    imagepos=[]
-    if pbPokerus(pokemon)==1 || pokemon.hp==0 || @pokemon.status>0
-      status=6 if pbPokerus(pokemon)==1
-      status=@pokemon.status-1 if @pokemon.status>0
-      status=5 if pokemon.hp==0
-      imagepos.push(["Graphics/Pictures/statuses",124,100,0,16*status,44,16])
-    end
-    if pokemon.isShiny?
-      imagepos.push([sprintf("Graphics/Pictures/shiny"),2,134,0,0,-1,-1])
-    end
-    if pbPokerus(pokemon)==2
-      imagepos.push([
-          sprintf("Graphics/Pictures/summaryPokerus"),176,100,0,0,-1,-1])
-    end
-    ballused=@pokemon.ballused ? @pokemon.ballused : 0
-    ballimage=sprintf("Graphics/Pictures/summaryball%02d",@pokemon.ballused)
-    imagepos.push([ballimage,14,60,0,0,-1,-1])
-    pbDrawImagePositions(overlay,imagepos)
     base=Color.new(248,248,248)
     shadow=Color.new(104,104,104)
-    pbSetSystemFont(overlay)
-    naturename=PBNatures.getName(pokemon.nature)
-    itemname=pokemon.item==0 ? _INTL("None") : PBItems.getName(pokemon.item)
-    pokename=@pokemon.name
-    if @pokemon.name.split('').last=="♂" || @pokemon.name.split('').last=="♀"
-      pokename=@pokemon.name[0..-2]
-    end
-    textpos=[
-      [_INTL("FAMILY TREE"),26,16,0,base,shadow],
-      [pokename,46,62,0,base,shadow],
-      [_INTL("Item"),16,320,0,base,shadow],
-      [itemname,16,352,0,Color.new(64,64,64),Color.new(176,176,176)],
-    ]
-    textpos.push([_INTL("{1}",pokemon.level),46,92,0,
-          Color.new(64,64,64),Color.new(176,176,176)]) if !@pokemon.isEgg?
-    if !@pokemon.isEgg?
-      if pokemon.gender==0
-        textpos.push([_INTL("♂"),178,62,0,
-            Color.new(24,112,216),Color.new(136,168,208)])
-      elsif pokemon.gender==1
-        textpos.push([_INTL("♀"),178,62,0,
-            Color.new(248,56,32),Color.new(224,152,144)])
+    textpos=[]
+    if @pokemon.isEgg?
+      overlay.clear
+      pbSetSystemFont(overlay)
+      @sprites["background"].setBitmap("Graphics/Pictures/Summary/bg_6_egg")
+      imagepos = []
+      ballimage = sprintf(
+        "Graphics/Pictures/Summary/icon_ball_%02d",@pokemon.ballused)
+      imagepos.push([ballimage,14,60,0,0,-1,-1])
+      pbDrawImagePositions(overlay,imagepos)
+      textpos=[
+         [_INTL("TRAINER MEMO"),26,16,0,base,shadow],
+         [@pokemon.name,46,62,0,base,shadow],
+         [_INTL("Item"),62,318,0,base,shadow]
+      ]
+      if @pokemon.hasItem?
+        textpos.push([PBItems.getName(@pokemon.item),16,352,0,
+          Color.new(64,64,64), Color.new(176,176,176)])
+      else
+        textpos.push([_INTL("None"),16,352,0,
+          Color.new(184,184,160),Color.new(208,208,200)])
       end
-    end    
-    base=Color.new(248,248,248)
-    shadow=Color.new(104,104,104)
+      drawMarkings(overlay,82,292)
+    end  
     # Draw parents
     parentsY=[78,234]
     for i in 0...2
@@ -115,11 +171,11 @@ class PokemonSummaryScene
           textpos.push([_INTL("♀"),500,32+parentsY[i],1,
               Color.new(248,56,32),Color.new(224,152,144)])
         end
-      end    
+      end
       grandX = [380,448]
       for j in 0...2
         iconGrandParam = parent && parent[j] ? [parent[j].species,
-            parent[j].gender==1,false,parent[j].form,false] : 
+            parent[j].gender==1,false,parent[j].form,false] :
             [0,0,false,0,false]
         iconGrand=AnimatedBitmap.new(pbCheckPokemonIconFiles(iconGrandParam))
         overlay.blt(
@@ -127,52 +183,48 @@ class PokemonSummaryScene
       end
     end
     pbDrawTextPositions(overlay,textpos)
-    drawMarkings(overlay,15,291,72,20,pokemon.markings)
   end
-  
+
   def handleInputsEgg
     if SHOWFAMILYEGG && @pokemon.isEgg?
-      if Input.trigger?(Input::LEFT) && @page==5
-        @page=0 
+      if Input.trigger?(Input::LEFT) && @page==6
+        @page=1
         pbPlayCursorSE()
         dorefresh=true
       end
-      if Input.trigger?(Input::RIGHT) && @page==0
-        @page=5 
+      if Input.trigger?(Input::RIGHT) && @page==1
+        @page=6
         pbPlayCursorSE()
         dorefresh=true
       end
     end
     if dorefresh
       case @page
-        when 0
-          drawPageOne(@pokemon)
-        when 5
-          drawPageSix(@pokemon)
+        when 1; drawPageOneEgg
+        when 6; drawPageSix
       end
     end
   end
 end
 
-
 class PokemonFamily
   MAXGENERATIONS = 3 # Tree stored generation limit
-  
+
   attr_reader :mother # PokemonFamily object
   attr_reader :father # PokemonFamily object
-  
+
   attr_reader :species
   attr_reader :gender
   attr_reader :form
   attr_reader :name # nickname
-  # You can add more data here and on initialize class. Just 
+  # You can add more data here and on initialize class. Just
   # don't store the entire pokémon object.
-  
+
   def initialize(pokemon, father=nil,mother=nil)
     initializedAsParent = !father || !mother
     if pokemon.family && pokemon.family.father
       @father = pokemon.family.father
-    elsif father 
+    elsif father
       @father = PokemonFamily.new(father)
     end
     if pokemon.family && pokemon.family.mother
@@ -180,18 +232,18 @@ class PokemonFamily
     elsif mother
       @mother = PokemonFamily.new(mother)
     end
-    
+
     # This data is only initialized as a parent in a cub.
-    if initializedAsParent 
+    if initializedAsParent
       @species=pokemon.species
       @gender=pokemon.gender
       @name=pokemon.name
       @form=pokemon.form
     end
-    
+
     applyGenerationLimit(MAXGENERATIONS)
   end
-  
+
   def applyGenerationLimit(generation)
     if generation>1
       father.applyGenerationLimit(generation-1) if @father
@@ -199,9 +251,9 @@ class PokemonFamily
     else
       father=nil
       mother=nil
-    end  
-  end 
-  
+    end
+  end
+
   def [](value) # [0] = father, [1] = mother
     if value==0
     return @father
@@ -210,8 +262,8 @@ class PokemonFamily
     end
     return nil
   end
-end  
-  
+end 
+
 class PokeBattle_Pokemon
   attr_accessor :family
 end
